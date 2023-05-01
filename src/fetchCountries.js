c66e9d3f711876a39287482b6c6fa98062bbaf8f
Liliia29 <118195732+Liliia29@users.noreply.github.com>
2023-05-01 21:42:45 +0300

    js-2-10

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,28 +1,13 @@
-function fetchCountries(name) {
-  const BASE_URL = 'https://restcountries.com/v3.1';
-
-  return fetch(
-    `${BASE_URL}/name/${name}?fields=capital?fields=population?fields=languages?fields=flags?fields=name`
-  ).then(response => {
-    if (!response.ok) {
-      if (response.status === 404) {
-        return [];
-      }
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-}
-export { fetchCountries };
-
-// const fields = 'fields=name,capital,languages,population,flags';
-// export const fetchCountries = async name => {
-//   const response = await fetch(`${BASE_URL}/name/${name}?${fields}`);
-//   if (!response.ok) {
-//     if (response.status === 404) {
-//       return [];
-//     }
-//     throw new Error(response.status);
-//   }
-//   return response.json();
-// };
+function fetchCountries(searchQuery) {
+    return fetch(`https://restcountries.com/v3.1/name/${searchQuery}`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Error fetching data');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  export { fetchCountries };
